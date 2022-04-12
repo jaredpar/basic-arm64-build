9d978041f5ee439e9f5715394702f32bc81cecdd
Jared Parsons <jaredpparsons@gmail.com>
2022-04-11 20:45:54 -0700

    Inner loop testing

--- src/ExampleExe/test.ps1
+++ src/ExampleExe/test.ps1
@@ -2,13 +2,19 @@
 try {
   Push-Location $PSScriptRoot
 
+  $packages = Join-Path $PSScriptRoot ".nuget\packages"
+  Remove-Item -Recurse $packages -ErrorAction SilentlyContinue
+  Remove-Item -Recurse bin,obj -ErrorAction SilentlyContinue
+
+  Create-Directory $packages
+  ${env:NUGET_PACKAGES}=$packages
+
   gps dotnet -ErrorAction SilentlyContinue | kill
 
-  & dotnet pack -nologo ..\Basic.Arm64.Build -p:Version=0.0.0.1
-
-  $restoreSource = Join-Path $PSScriptRoot "..\Basic.Arm64.Build\bin\Debug"
-  & dotnet build -nologo -p:RestoreSources="$restoreSource"
+  & dotnet pack -restore -nologo ..\Basic.Arm64.Build -p:Version=0.0.0.1
+  & dotnet build -nologo -bl -p:RestoreAdditionalProjectSources="..\Basic.Arm64.Build\bin\Debug"
 }
 finally {
+  Remove-Item env:\NUGET_PACKAGES
   Pop-Location
 }
